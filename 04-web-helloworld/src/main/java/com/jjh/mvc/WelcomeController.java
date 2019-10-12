d848bf461c03f54ed117eb493fc7d3fae78e30bd
John Hunt <john@matraining.com>
2019-10-12 16:11:20 +0100

    SImple web example

---
+++ 04-web-helloworld/src/main/java/com/jjh/mvc/WelcomeController.java
@@ -0,0 +1,30 @@
+package com.jjh.mvc;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("welcome")
+public class WelcomeController {
+
+	// http://localhost:8080/welcome/hello
+	
+	@GetMapping("hello")
+	public String sayhello(Model model) throws Exception {
+		System.out.println("WelcomeController.hello()");
+		model.addAttribute("message", "John - " + new Date());
+		return "greeting";
+	}
+
+	// use http://localhost:8080/welcome/help
+	
+	@GetMapping("help")
+	public String getHelp() {
+		System.out.println("WelcomeController.help()");
+		return "info";
+	}
+}
