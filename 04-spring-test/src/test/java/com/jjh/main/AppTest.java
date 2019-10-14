090436a8b840d85092fc0eb8ebbcb54b4ad7ebbe
John Hunt <john@matraining.com>
2019-10-14 15:53:46 +0100

    Added a couple of application level tests

--- 04-spring-test/src/test/java/com/jjh/main/AppTest.java
+++ 04-spring-test/src/test/java/com/jjh/main/AppTest.java
@@ -1,20 +1,31 @@
 package com.jjh.main;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AppTest  {
+public class AppTest {
 
-	  @Test
-	  public void contextLoads() {
-	  }
-	  
-	  
-	  
-	  
+	@Autowired
+	ApplicationContext context;
+
+	@Test
+	public void contextLoads() {
+		assertNotNull(context);
+	}
+	
+	@Test
+	public void checkAllBeansLoaded() {
+		int totalbeans = this.context.getBeanDefinitionCount();
+		assertEquals("There should be 26 beans loaded", 26, totalbeans);
+	}
+
 }
