006131446bfacbf9707b26e3d24114c1b8bee3dc
John Hunt <john@matraining.com>
2019-10-14 14:29:31 +0100

    Spring Messaging Example

---
+++ 07-spring-messaging/src/main/java/com/jjh/bookshop/main/App.java
@@ -0,0 +1,31 @@
+package com.jjh.bookshop.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.jjh.bookshop.domain.Book;
+
+@SpringBootApplication
+@EnableJms
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting");
+		ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+
+		Sender sender = context.getBean(Sender.class);
+
+		// Publish a book
+		System.out.println("Sending an book message.");
+
+		Book book = new Book("1", "Java Forever", "Jasmine Smith", 12.55);
+
+		sender.send(book);
+
+		System.out.println("Press Enter to Terminate");
+		System.in.read();
+
+	}
+}
