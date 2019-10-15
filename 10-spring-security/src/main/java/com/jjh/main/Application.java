06f40a5e308b108bfa58dec2b0cb3c6760d89ee3
John Hunt <john@matraining.com>
2019-10-15 09:18:25 +0100

    Basic Spring Security Example

---
+++ 10-spring-security/src/main/java/com/jjh/main/Application.java
@@ -0,0 +1,18 @@
+package com.jjh.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+// May need to turn off security auto-configuration to do it all ourselves
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+    	System.out.println("Starting the Hello World web application");
+        SpringApplication.run(Application.class, args);
+        System.out.println("Startup complete");
+    }
+}
---
+++ 10-spring-security/src/main/java/com/jjh/main/BasicSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.jjh.main;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackages = "com.jjh.controller")
+public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	public BasicSecurityConfiguration() {
+		System.out.println("BasicSecurityConfiguration.<init>()");
+	}
+ 
+	// Can also use the following method to set up a custom AuthenticationProvider
+	// auth.authenticationProvider(new MyCustomAuthenticationProvider());
+	// This must implement the AuthenticationProvider interface
+	
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+      throws Exception {
+    	System.out.println("BasicSecurityConfiguration.configure(auth)");
+        auth
+          .inMemoryAuthentication()
+          .withUser("jeh")
+            .password("{noop}user123") // Otherwise expects encoded password
+            .roles("USER")
+            .and()
+          .withUser("admin")
+            .password("{noop}admin")
+            .roles("USER", "ADMIN");
+    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	System.out.println("BasicSecurityConfiguration.configure(http)");
+        http
+          .csrf().disable()     // Cross-Site Request Forgery protection turned on by default (see also .cors() Cross Origin Requests)
+          .authorizeRequests()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic();
+    }
+}
+
