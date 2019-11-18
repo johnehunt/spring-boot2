0199f42122255d415129bca8137d90efa7419e98
John Hunt <john@matraining.com>
2019-11-18 14:01:57 +0000

    Removed unused input

--- 05-dependency-injection/src/main/java/com/jjh/main/HelloApplication.java
+++ 05-dependency-injection/src/main/java/com/jjh/main/HelloApplication.java
@@ -7,24 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.jjh.beans.MessageService;
 import com.jjh.beans.WelcomeService;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.jjh.beans")
 public class HelloApplication {
-	
 
 	public static void main(String[] args) {
 		System.out.println("Main - obtaining Application Context");
 		ApplicationContext ctx = SpringApplication.run(HelloApplication.class, args);
-		
+
 		System.out.println("Main - requesting MessageService");
-		WelcomeService service = (WelcomeService)ctx.getBean(WelcomeService.class);
-		
+		WelcomeService service = (WelcomeService) ctx.getBean(WelcomeService.class);
+
 		String result = service.getWelcomeInfo("John", new Date());
 		System.out.println("Result: " + result);
-		
+
 	}
 
 }
