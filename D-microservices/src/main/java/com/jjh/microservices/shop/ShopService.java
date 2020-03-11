e3734f926c1bc1d5f00fe8e67d722304782309d9
John Hunt <jeh@midmarsh.co.uk>
2020-03-11 17:27:46 +0000

    Fixed error in comment

--- D-microservices/src/main/java/com/jjh/microservices/shop/ShopService.java
+++ D-microservices/src/main/java/com/jjh/microservices/shop/ShopService.java
@@ -1,4 +1,4 @@
-package com.jjh.mircoservcies.shop;
+package com.jjh.microservices.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,37 +12,21 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @Configuration
-@ComponentScan(useDefaultFilters=false)  // Disable component scanner
+@ComponentScan
 public class ShopService {
-	
-	public static final String USER_SERVICE_URL = "http://book-service";
-	
+
 	public static void main(String[] args) {
 		System.out.println("Starting the BookShop Service");
-        // Will configure using web-server.yml
-        System.setProperty("spring.config.name", "shop-service");
-        SpringApplication.run(ShopService.class, args);
-        System.out.println("Bookshop Service started");
-    }
+		// Will configure using shop-service.yml
+		System.setProperty("spring.config.name", "shop-service");
+		SpringApplication.run(ShopService.class, args);
+		System.out.println("Bookshop Service started");
+	}
 	
 	@LoadBalanced    // Make sure to create the load-balanced template
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    /**
-     * Account service calls microservice internally using provided URL.
-     */
-    @Bean
-    public BookServiceDelegate bookService() {
-        return new BookServiceDelegate(USER_SERVICE_URL);
-    }
-
-    @Bean
-    public ShopController bookshopController() {
-         return new ShopController
-                       (bookService());  // plug in account-service
     }
 
 }
