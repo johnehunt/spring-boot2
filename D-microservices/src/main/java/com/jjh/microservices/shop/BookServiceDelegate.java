29659e1077f6c5f735299d1593ef1c83c305b14a
John Hunt <jeh@midmarsh.co.uk>
2020-03-11 17:26:01 +0000

    Simplification of the implementation

--- D-microservices/src/main/java/com/jjh/microservices/shop/BookServiceDelegate.java
+++ D-microservices/src/main/java/com/jjh/microservices/shop/BookServiceDelegate.java
@@ -1,28 +1,25 @@
-package com.jjh.mircoservcies.shop;
+package com.jjh.microservices.shop;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Component;
 
 import com.jjh.microservices.books.Book;
 
+@Component
 public class BookServiceDelegate {
+
+	public static final String USER_SERVICE_URL = "http://book-service";
 
 	@Autowired // NO LONGER auto-created by Spring Cloud (see below)
 	@LoadBalanced // Explicitly request the load-balanced template
-					// with Ribbon built-in
 	private RestTemplate restTemplate;
 
-	private String serviceUrl;
-
-	public BookServiceDelegate(String serviceUrl) {
-		this.serviceUrl = serviceUrl;
-	}
-
 	public List<Book> getBooks() {
-		List<Book> results = (List<Book>) restTemplate.getForObject(serviceUrl, List.class);
+		List<Book> results = (List<Book>) restTemplate.getForObject(USER_SERVICE_URL, List.class);
 		return results;
 	}
 
