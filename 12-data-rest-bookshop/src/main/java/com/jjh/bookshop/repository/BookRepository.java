77c1e33f4f59c18fee90fb384c1cdbc019ff4c7d
John Hunt <john.hunt10@gmail.com>
2020-08-05 17:10:55 +0100

    Restructured to match course change

--- 12-data-rest-bookshop/src/main/java/com/jjh/bookshop/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jjh.bookshop.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.jjh.bookshop.domain.Book;
-
-@Repository
-public interface BookRepository extends CrudRepository<Book, String> {}
