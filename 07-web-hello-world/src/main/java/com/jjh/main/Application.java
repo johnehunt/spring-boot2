7fe4ece18e1daa530e4ddd1ea3969759351369ee
John Hunt <john.hunt10@gmail.com>
2020-08-06 11:10:43 +0100

    Added additional print statements

--- 07-web-hello-world/src/main/java/com/jjh/main/Application.java
+++ 07-web-hello-world/src/main/java/com/jjh/main/Application.java
@@ -10,7 +10,8 @@ public class Application {
     public static void main(String[] args) {
     	System.out.println("Starting the web application");
         SpringApplication.run(Application.class, args);
-        System.out.println("Startup complete");
-        System.out.println("See - http://localhost:8080/welcome/hello");
+        System.out.println("Server Startup complete");
+        System.out.println("\tSee - http://localhost:8080/welcome/hello");
+        System.out.println("\tSee - http://localhost:8080/welcome/help");
     }
 }
