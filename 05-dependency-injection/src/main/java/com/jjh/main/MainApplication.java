dd8f3c8b60a6712616126e136bd3b4e735987ced
John Hunt <john.hunt10@gmail.com>
2020-08-06 10:52:31 +0100

    Added example of primary annotation for autowiring

--- 05-dependency-injection/src/main/java/com/jjh/main/MainApplication.java
+++ 05-dependency-injection/src/main/java/com/jjh/main/MainApplication.java
@@ -2,6 +2,8 @@ package com.jjh.main;
 
 import java.util.Date;
 
+import com.jjh.primary.FoodService;
+import com.jjh.primary.House;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,18 +12,21 @@ import org.springframework.context.annotation.ComponentScan;
 import com.jjh.beans.WelcomeService;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.jjh.beans")
-public class HelloApplication {
+@ComponentScan(basePackages = "com.jjh.beans,com.jjh.primary")
+public class MainApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Main - obtaining Application Context");
-		ApplicationContext ctx = SpringApplication.run(HelloApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(MainApplication.class, args);
 
 		System.out.println("Main - requesting MessageService");
 		WelcomeService service = ctx.getBean(WelcomeService.class);
 
 		String result = service.getWelcomeInfo("John", new Date());
 		System.out.println("Result: " + result);
+
+		House house = ctx.getBean(House.class);
+		house.supplyFood();
 
 	}
 
