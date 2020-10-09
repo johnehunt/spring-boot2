3fc2835646284f4d0c5ee99ff936e7ef5086833a
John Hunt <john.hunt10@gmail.com>
2020-10-09 16:49:18 +0100

    Removed unused import

--- 11-spring-messaging/src/main/java/com/jjh/bookshop/main/SenderConfig.java
+++ 11-spring-messaging/src/main/java/com/jjh/bookshop/main/SenderConfig.java
@@ -1,7 +1,6 @@
 package com.jjh.bookshop.main;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
