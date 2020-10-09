0ba5b8cdba01bb9439f07932aaed40b66e4a5829
John Hunt <john.hunt10@gmail.com>
2020-10-09 18:12:58 +0100

    Custom login page

--- 13-spring-security/src/main/java/com/jjh/main/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.jjh.main;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.jjh.controller")
-public class AppConfig {}
