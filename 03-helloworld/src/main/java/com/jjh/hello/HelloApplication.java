f56d7324c327d940eef64446299859da38ea3c5b
John Hunt <john.hunt10@gmail.com>
2021-09-17 16:00:25 +0100

    Updated for latest spring boot

--- 03-helloworld/src/main/java/com/jjh/hello/HelloApplication.java
+++ 03-helloworld/src/main/java/com/jjh/hello/HelloApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Up");
 		SpringApplication.run(HelloApplication.class, args);
 		System.out.println("Hello World");
 	}
