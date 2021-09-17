256547d4352514c06455134614fee0cb8d5757c4
John Hunt <john.hunt10@gmail.com>
2021-09-17 16:13:19 +0100

    Updated for latest spring boot

--- 05-dependency-injection/src/main/java/com/jjh/main/Application.java
+++ 05-dependency-injection/src/main/java/com/jjh/main/Application.java
@@ -12,7 +12,10 @@ import org.springframework.context.annotation.ComponentScan;
 import com.jjh.beans.WelcomeService;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.jjh.beans,com.jjh.primary,com.jjh.writing")
+@ComponentScan(basePackages = {
+		"com.jjh.beans",
+		"com.jjh.primary",
+		"com.jjh.writing"})
 public class Application {
 
 	public static void main(String[] args) {
