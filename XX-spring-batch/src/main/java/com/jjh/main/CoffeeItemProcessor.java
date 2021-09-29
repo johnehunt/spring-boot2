d3d8d6c916c7dbc48e2e0393b7b3ce12631c47c9
John Hunt <john.hunt10@gmail.com>
2021-09-29 14:52:31 +0100

    Added a spring BNatch example

---
+++ XX-spring-batch/src/main/java/com/jjh/main/CoffeeItemProcessor.java
@@ -0,0 +1,23 @@
+package com.jjh.main;
+
+import com.jjh.domain.Coffee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CoffeeItemProcessor implements ItemProcessor<Coffee, Coffee> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CoffeeItemProcessor.class);
+
+    @Override
+    public Coffee process(final Coffee coffee) throws Exception {
+        String brand = coffee.getBrand().toUpperCase();
+        String origin = coffee.getOrigin().toUpperCase();
+        String characteristics = coffee.getCharacteristics().toUpperCase();
+
+        Coffee transformedCoffee = new Coffee(brand, origin, characteristics);
+        LOGGER.info("Converting ( {} ) into ( {} )", coffee, transformedCoffee);
+
+        return transformedCoffee;
+    }
+}
